ee044328fbecc930b8b4098532a519645e0ddeef
suiyujin <suiyujin04@gmail.com>
2016-06-06 00:05:20 +0900

    create setting for unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true  # 更新時ダウンタイム無し
+
+listen "/var/www/flick_typing/shared/tmp/sockets/unicorn.sock"
+pid "/var/www/flick_typing/shared/tmp/pids/unicorn.pid"
+
+working_directory "/var/www/flick_typing/current"
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
+
+# ログの出力
+stderr_path File.expand_path('log/unicorn_stderr.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn_stdout.log', ENV['RAILS_ROOT'])
