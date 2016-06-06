4d4b080a55adc519ce578d40e0e971599639a4a5
suiyujin <suiyujin04@gmail.com>
2016-06-07 02:41:09 +0900

    make seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+def make_dummy_data(data)
+  data[1..-1].map { |d| Hash[*data[0].zip(d).flatten] }
+end
+
+# teams
+Team.create(make_dummy_data(CSV.read('db/seed_data/teams.csv')))
+p 'teams inserted.'
+
+# answers
+Answer.create(make_dummy_data(CSV.read('db/seed_data/answers.csv')))
+p 'answers inserted.'
